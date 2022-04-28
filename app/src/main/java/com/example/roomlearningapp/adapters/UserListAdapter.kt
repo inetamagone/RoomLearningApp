2375edaa5f81b6ead8bdeb1473e09f6fc7a9324f
Maksims Samsonovs <maksims.samsonovs@accenture.com>
2022-04-28 18:33:20 +0300

    code improvements suggestions

--- app/src/main/java/com/example/roomlearningapp/adapters/UserListAdapter.kt
+++ app/src/main/java/com/example/roomlearningapp/adapters/UserListAdapter.kt
@@ -32,10 +32,13 @@ class UserListAdapter(private val userList: List<UserModel>, private val context
     inner class UserViewHolder(private val binding: ListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(userModel: UserModel) {
-            binding.surname.text = userModel.firstName
-            binding.name.text = userModel.lastName
-            binding.menuItem
-                .setOnLongClickListener { popupMenus(it, this) }
+
+            // instead of binding. binding. binding. much better to use binding.apply {}
+            binding.apply {
+                surname.text = userModel.firstName
+                name.text = userModel.lastName
+                menuItem.setOnLongClickListener { popupMenus(it, this@UserViewHolder) }
+            }
 
             if (userModel.colorPriority == 0) {
                 binding.textContainer
@@ -48,141 +51,168 @@ class UserListAdapter(private val userList: List<UserModel>, private val context
 
         // Item menu
         private fun popupMenus(view: View, viewHolder: UserViewHolder): Boolean {
-            val popupMenus = PopupMenu(context, view)
-            popupMenus.inflate(R.menu.item_menu)
-            popupMenus.setOnMenuItemClickListener {
+            //again .apply{} can do code better by removing not necessary variable and lot of popupMenus. popupMenus. popupMenus.
+            PopupMenu(context, view).apply {
+                inflate(R.menu.item_menu)
+                setOnMenuItemClickListener {
+
+/*
+                Code will be much more readable if you will do it like this. (Define separate functions)
+
                 when (it.itemId) {
-                    R.id.move_up -> {
-                        val currentItem = getItemByID(viewHolder.adapterPosition)
-                        val currentItemId = currentItem.id
-                        val currentPosition = viewHolder.adapterPosition
+                    R.id.move_up -> moveItemUp()
+                    R.id.move_down -> moveItemDown()
+                    R.id.remove_user -> removeUser()
+                }
 
-                        if (currentPosition != 0) {
-                            val currentFirstName = currentItem.firstName
-                            val currentLastName = currentItem.lastName
-                            val currentColor = currentItem.colorPriority
+                Also I didn't check your moveUp/Down realisation but pretty sure it can be done by one common function something like this:
 
-                            val itemBPosition = currentPosition - 1
-                            val itemB = getItemByID(itemBPosition)
-                            val itemBid = itemB.id
-                            val itemBFirstName = itemB.firstName
-                            val itemBLastName = itemB.lastName
-                            val itemBColor = itemB.colorPriority
+                fun moveItem (isDirectionUp: Boolean) {
+                    if (isDirectionUp) {up direction piece of code}
+                    else {down direction piece of code}
+                    common code
+                }
 
-                            currentItem.firstName = itemBFirstName
-                            currentItem.lastName = itemBLastName
-                            currentItem.colorPriority = itemBColor
+                actually even more readable will be introduce enum direction here instead of the Boolean but let it left
+                Boolean for now and I'll show how to change to enum and how to use enum if you not used them before
 
-                            itemB.firstName = currentFirstName
-                            itemB.lastName = currentLastName
-                            itemB.colorPriority = currentColor
 
-                            viewModel.updateUser(
-                                itemBFirstName,
-                                itemBLastName,
-                                itemBColor,
-                                currentItemId
-                            )
-                            viewModel.updateUser(
-                                currentFirstName,
-                                currentLastName,
-                                currentColor,
-                                itemBid
-                            )
-                            notifyDataSetChanged()
-                        } else {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.list_top_message),
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
-                        }
-                        true
-                    }
-                    R.id.move_down -> {
-                        val listSize = userList.size
-                        val currentItem = getItemByID(viewHolder.adapterPosition)
-                        val currentItemId = currentItem.id
-                        val currentPosition = viewHolder.adapterPosition
+                */
 
-                        if (currentPosition < (listSize - 1)) {
-                            val currentFirstName = currentItem.firstName
-                            val currentLastName = currentItem.lastName
-                            val currentColor = currentItem.colorPriority
+                    when (it.itemId) {
+                        R.id.move_up -> {
+                            val currentItem = getItemByID(viewHolder.adapterPosition)
+                            val currentItemId = currentItem.id
+                            val currentPosition = viewHolder.adapterPosition
 
-                            val itemBPosition = currentPosition + 1
-                            val itemB = getItemByID(itemBPosition)
-                            val itemBId = itemB.id
-                            val itemBFirstName = itemB.firstName
-                            val itemBLastName = itemB.lastName
-                            val itemBColor = itemB.colorPriority
+                            if (currentPosition != 0) {
+                                val currentFirstName = currentItem.firstName
+                                val currentLastName = currentItem.lastName
+                                val currentColor = currentItem.colorPriority
 
-                            currentItem.firstName = itemBFirstName
-                            currentItem.lastName = itemBLastName
-                            currentItem.colorPriority = itemBColor
+                                val itemBPosition = currentPosition - 1
+                                val itemB = getItemByID(itemBPosition)
+                                val itemBid = itemB.id
+                                val itemBFirstName = itemB.firstName
+                                val itemBLastName = itemB.lastName
+                                val itemBColor = itemB.colorPriority
 
-                            itemB.firstName = currentFirstName
-                            itemB.lastName = currentLastName
-                            itemB.colorPriority = currentColor
+                                currentItem.firstName = itemBFirstName
+                                currentItem.lastName = itemBLastName
+                                currentItem.colorPriority = itemBColor
 
-                            viewModel.updateUser(
-                                itemBFirstName,
-                                itemBLastName,
-                                itemBColor,
-                                currentItemId
-                            )
-                            viewModel.updateUser(
-                                currentFirstName,
-                                currentLastName,
-                                currentColor,
-                                itemBId
-                            )
-                            notifyDataSetChanged()
-                        } else {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.list_bottom_message),
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
-                        }
-                        true
-                    }
-                    R.id.remove_user -> {
-                        AlertDialog.Builder(context)
-                            .setPositiveButton(context.resources.getString(R.string.ok)) { dialog, _ ->
-                                viewModel.deleteUser(getItemByID(viewHolder.adapterPosition))
-                                notifyItemRemoved(viewHolder.adapterPosition)
+                                itemB.firstName = currentFirstName
+                                itemB.lastName = currentLastName
+                                itemB.colorPriority = currentColor
+
+                                viewModel.updateUser(
+                                    itemBFirstName,
+                                    itemBLastName,
+                                    itemBColor,
+                                    currentItemId
+                                )
+                                viewModel.updateUser(
+                                    currentFirstName,
+                                    currentLastName,
+                                    currentColor,
+                                    itemBid
+                                )
+                                notifyDataSetChanged()
+                            } else {
                                 Toast.makeText(
                                     context,
-                                    context.getString(R.string.user_deleted),
+                                    context.getString(R.string.list_top_message),
                                     Toast.LENGTH_SHORT
                                 )
                                     .show()
-                                dialog.dismiss()
                             }
-                            .setNegativeButton(context.getString(R.string.no)) { dialog, _ ->
-                                dialog.dismiss()
+                            true
+                        }
+                        R.id.move_down -> {
+                            val listSize = userList.size
+                            val currentItem = getItemByID(viewHolder.adapterPosition)
+                            val currentItemId = currentItem.id
+                            val currentPosition = viewHolder.adapterPosition
+
+                            if (currentPosition < (listSize - 1)) {
+                                val currentFirstName = currentItem.firstName
+                                val currentLastName = currentItem.lastName
+                                val currentColor = currentItem.colorPriority
+
+                                val itemBPosition = currentPosition + 1
+                                val itemB = getItemByID(itemBPosition)
+                                val itemBId = itemB.id
+                                val itemBFirstName = itemB.firstName
+                                val itemBLastName = itemB.lastName
+                                val itemBColor = itemB.colorPriority
+
+                                currentItem.firstName = itemBFirstName
+                                currentItem.lastName = itemBLastName
+                                currentItem.colorPriority = itemBColor
+
+                                itemB.firstName = currentFirstName
+                                itemB.lastName = currentLastName
+                                itemB.colorPriority = currentColor
+
+                                viewModel.updateUser(
+                                    itemBFirstName,
+                                    itemBLastName,
+                                    itemBColor,
+                                    currentItemId
+                                )
+                                viewModel.updateUser(
+                                    currentFirstName,
+                                    currentLastName,
+                                    currentColor,
+                                    itemBId
+                                )
+                                notifyDataSetChanged()
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.list_bottom_message),
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
                             }
-                            .setTitle(context.getString(R.string.delete))
-                            .setIcon(R.drawable.warning)
-                            .setMessage(context.getString(R.string.delete_this_user))
-                            .create()
-                            .show()
-                        true
+                            true
+                        }
+                        R.id.remove_user -> {
+                            AlertDialog.Builder(context)
+                                .setPositiveButton(context.resources.getString(R.string.ok)) { dialog, _ ->
+                                    viewModel.deleteUser(getItemByID(viewHolder.adapterPosition))
+                                    notifyItemRemoved(viewHolder.adapterPosition)
+                                    Toast.makeText(
+                                        context,
+                                        context.getString(R.string.user_deleted),
+                                        Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                    dialog.dismiss()
+                                }
+                                .setNegativeButton(context.getString(R.string.no)) { dialog, _ ->
+                                    dialog.dismiss()
+                                }
+                                .setTitle(context.getString(R.string.delete))
+                                .setIcon(R.drawable.warning)
+                                .setMessage(context.getString(R.string.delete_this_user))
+                                .create()
+                                .show()
+                            true
+                        }
+                        R.id.highlight_user -> {
+                            val selectedId = getItemByID(viewHolder.adapterPosition).id
+                            val currentColor = getItemByID(viewHolder.adapterPosition).colorPriority
+                            val newColor = changeColor(currentColor)
+                            viewModel.updateColor(newColor, selectedId)
+                            true
+                        }
+                        else -> true
                     }
-                    R.id.highlight_user -> {
-                        val selectedId = getItemByID(viewHolder.adapterPosition).id
-                        val currentColor = getItemByID(viewHolder.adapterPosition).colorPriority
-                        val newColor = changeColor(currentColor)
-                        viewModel.updateColor(newColor, selectedId)
-                        true
-                    }
-                    else -> true
                 }
+                show()
             }
-            popupMenus.show()
+
             return true
         }
     }
@@ -195,6 +225,10 @@ class UserListAdapter(private val userList: List<UserModel>, private val context
         return userList.size
     }
 
+    //Poor piece of code.
+    // 1. it can be simplified by using Kotlin "expression body"
+    // 2. if it return only 0 or 1 it should be boolean I suppouse
+    // 3. Don't want to go deep in code but I don't think that variable name currentColor represent value stored. and function name changeColor also doesn't represent it's functionality.
     fun changeColor(currentColor: Int): Int {
         return if (currentColor == 0) {
             1
